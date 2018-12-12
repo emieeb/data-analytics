d2af922b32d63d65b289285a11b40c506f32a549
Amelia Berumen <ameliaberumen@amelias-mbp.attlocal.net>
2018-12-11 23:19:50 -0800

    mysql

---
+++ MYSQLHOMEWRK.sql
@@ -0,0 +1,19 @@
+USE sakila;
+
+SELECT first_name, last_name FROM actor;
+
+SELECT CONCAT(first_name,' ', last_name) 
+	AS Actor_Name FROM actor;
+
+SELECT * FROM actor WHERE first_name = "Joe";
+
+SELECT * FROM actor 
+	WHERE last_name LIKE '%GEN%';
+
+SELECT * FROM actor 
+	WHERE last_name LIKE '%LI%'
+	ALTER TABLE actor
+	ADD COLUMN last_name INT AUTO_INCREMENT PRIMARY KEY FIRST;
+    
+
+    
